6f2d6bbcb655f0579c8e50f32ec331ba731c0050
Anil Guney Altun <47734769+anilguneyaltun@users.noreply.github.com>
2025-02-12 04:37:00 +0000

    Delete dotdrop directory

--- dotdrop/dotfiles/config/polybar/temp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Example: extract the CPU temperature from lm-sensors output (adjust according to your sensor labels)
-temp=$(sensors | awk '/^Core 0/ { gsub(/\+|°C/, "", $3); print $3; exit }')
-echo "CPU: ${temp}°C"
